d9baf83095ad69f814df2151a94ec3b3d55a6dd1
danielbr <danielbradham@gmail.com>
2014-07-09 13:26:30 -0400

    Updated

--- nbackgrounder/utils.py
+++ nbackgrounder/utils.py
@@ -40,6 +40,7 @@ def execute(script):
         }[plat]
         subprocess.call([starter, script])
 
+
 @contextlib.contextmanager
 def ShellScript(mesh_only=False, post_mesh=False, timeout=10):
     try:
@@ -53,56 +54,65 @@ def ShellScript(mesh_only=False, post_mesh=False, timeout=10):
         except OSError:
             print "Could not remove {0}".format(shell_script)
 
+
 def generate_shell_script(mesh_only=False, post_mesh=False, timeout=10):
     '''Generates an OS specific shell script.'''
     template = TEMPLATES.get(platform.system(), None)
     if not TEMPLATES:
         raise OSError("Can not find a shell script template for your OS.")
 
-    #Get the appropriate data from current Maya session
-    filename = cmds.file(query=True, sceneName=True)
-    file_prefix = os.path.splitext(os.path.basename(filename))[0]
-    if mesh:
-        cachedir = os.path.join(
-            cmds.workspace(q=True, rd=True),
-            cmds.workspace("Alembic", q=True, fre=True),
-            file_prefix.split(".")[0])
-    else:
-        cachedir = os.path.join(
-            cmds.workspace(q=True, rd=True),
-            cmds.workspace("fileCache", q=True, fre=True),
-            file_prefix.replace(".", "_"))
     particles = " ".join(['-p {0}'.format(n) for n in cmds.ls(sl=True)
                           if cmds.listRelatives(n, type="nParticle")])
     if not particles:
         cmds.headsUpMessage("Select the particle nodes you'd like to cache.")
         raise Exception("Select the particle nodes you'd like to cache.")
+
+    #Get the appropriate data from current Maya session
+    filename = cmds.file(query=True, sceneName=True)
+    file_prefix = os.path.splitext(os.path.basename(filename))[0]
+    mesh_cachedir = os.path.join(
+        cmds.workspace(q=True, rd=True),
+        cmds.workspace("Alembic", q=True, fre=True),
+        file_prefix.split(".")[0])
+    cachedir = os.path.join(
+        cmds.workspace(q=True, rd=True),
+        cmds.workspace("fileCache", q=True, fre=True),
+        file_prefix.replace(".", "_"))
     start_time, stop_time = (
         int(cmds.playbackOptions(query=True, min=True)),
         int(cmds.playbackOptions(query=True, max=True)))
 
-
     script_file = os.path.splitext(filename)[0] + ".bat"
-    script = formatter.format(
-        nback=NBACKGROUNDER_PY,
-        filename=filename,
-        cachedir=cachedir,
-        particles=particles,
-        start=start_time,
-        stop=stop_time,
-        mesh="-mesh" if mesh else "",
-        timeout=timeout)
-    if not mesh and post_mesh:
-        script.replace("sleep {0}".format(timeout), "sleep 10")
-        script.replace("timeout {0}".format(timeout), "timeout 10")
-        script += formatter.format(
+    if post_mesh:
+        script = formatter.format(
             nback=NBACKGROUNDER_PY,
             filename=filename,
             cachedir=cachedir,
             particles=particles,
             start=start_time,
             stop=stop_time,
+            mesh="",
+            timeout=timeout)
+        script.replace("sleep {0}".format(timeout), "sleep 10")
+        script.replace("timeout {0}".format(timeout), "timeout 10")
+        script += formatter.format(
+            nback=NBACKGROUNDER_PY,
+            filename=os.path.splitext(filename)[0] + ".bat",
+            cachedir=mesh_cachedir,
+            particles=particles,
+            start=start_time,
+            stop=stop_time,
             mesh="-mesh",
+            timeout=timeout)
+    else:
+        script = formatter.format(
+            nback=NBACKGROUNDER_PY,
+            filename=filename,
+            cachedir=cachedir if not mesh_only else mesh_cachedir,
+            particles=particles,
+            start=start_time,
+            stop=stop_time,
+            mesh="-mesh" if mesh_only else "",
             timeout=timeout)
 
     with open(script_file, 'w') as f:
